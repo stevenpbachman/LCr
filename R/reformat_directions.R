6ac96782bab1c6da1fe5d8210f5df4f138d78538
Steven Bachman <s.bachman@kew.org>
2025-05-05 19:33:21 +0100

    updated realms data file and cadinal directions function to handle SE

--- R/reformat_directions.R
+++ R/reformat_directions.R
@@ -8,19 +8,19 @@
 
 replace_cardinal_directions <- function(text) {
   # Create a mapping of abbreviations to full forms (lowercase)
-  replacements <- c(
-    "N\\." = "north",
-    "N\\.E\\." = "northeast", # Handle N.E. first before N.
+  replacements <- list(
+    "N\\.E\\." = "northeast",
+    "S\\.E\\." = "southeast",
+    "S\\.W\\." = "southwest",
+    "N\\.W\\." = "northwest",
     "NE\\." = "northeast",
-    "E\\." = "east",
-    "S\\." = "south",
-    "S\\.E\\." = "southeast", # Handle S.E. first before S.data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAkCAYAAAD7PHgWAAABBklEQVR4Xu2XMQrCQBBFBQvR6wgJHsEDpHVjBDvvoBhbI3bWCkZbFUyhFrYiEat0WgmC6AVkdQqbIVmWZAOi82C64b+/bDWZDEEQP4phTLMaa9d003bTGMgu1psF7JVGNzuWPdzs18GDz443rgrIcndXbvW8g1axGfZKo7P2eBXc+WB74a3FGXtiA1kwzfnpqTF7hL3SwDfAaz+BqvjkwYADe6WhglQwJlQwKVQwKakVTGOoYNL5z4JxwBlUMEwqAu9SwTCpCLxLBcOkIvCusoKT9/WFQ6OkIvCukoJwt5rO0sehUVIReBem6ng+OLBXmnKjn4PbGM5PeKnqgXIlo5vHXoL4Nl4ZYqbbEGA7+wAAAABJRU5ErkJggg==
     "SE\\." = "southeast",
-    "S\\.W\\." = "southwest", # Handle S.W. first before S.
     "SW\\." = "southwest",
-    "W\\." = "west",
-    "N\\.W\\." = "northwest", # Handle N.W. first before N.
     "NW\\." = "northwest",
+    "N\\." = "north",
+    "S\\." = "south",
+    "E\\." = "east",
+    "W\\." = "west",
     "C\\." = "central",
     "Trop\\." = "tropical",
     "Subtrop\\." = "subtropical"
