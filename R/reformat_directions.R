5b1109b4da6f75ba90daba6d26e137528d40b223
Steven Bachman <s.bachman@kew.org>
2025-05-09 10:46:52 +0100

    improved powo location formatting to tackle case and directions

--- R/reformat_directions.R
+++ R/reformat_directions.R
@@ -7,32 +7,64 @@
 
 
 replace_cardinal_directions <- function(text) {
-  # Create a mapping of abbreviations to full forms (lowercase)
+
+  # Replace ampersands with 'and'
+  text <- gsub(" *& *", " and ", text)
+
+  lowercase_terms = c("subalpine", "subarctic", "temperate","mountains", "tropical", "subtropical", "north", "south", "east", "west", "central",
+                      "northeast", "southeast", "northwest", "southwest",
+                      "north central", "south central", "east central", "west central",
+                      "northeast central", "southeast central", "northwest central", "southwest central")
+
   replacements <- list(
-    "N\\.E\\." = "northeast",
-    "S\\.E\\." = "southeast",
-    "S\\.W\\." = "southwest",
-    "N\\.W\\." = "northwest",
-    "NE\\." = "northeast",
-    "SE\\." = "southeast",
-    "SW\\." = "southwest",
-    "NW\\." = "northwest",
-    "N\\." = "north",
-    "S\\." = "south",
-    "E\\." = "east",
-    "W\\." = "west",
-    "C\\." = "central",
-    "Trop\\." = "tropical",
-    "Subtrop\\." = "subtropical"
+    "\\bN\\.E\\. C\\." = "northeast central",
+    "\\bS\\.E\\. C\\." = "southeast central",
+    "\\bS\\.W\\. C\\." = "southwest central",
+    "\\bN\\.W\\. C\\." = "northwest central",
+    "\\bN\\. C\\." = "north central",
+    "\\bS\\. C\\." = "south central",
+    "\\bE\\. C\\." = "east central",
+    "\\bW\\. C\\." = "west central",
+
+    "\\bN\\.E\\." = "northeast",
+    "\\bS\\.E\\." = "southeast",
+    "\\bS\\.W\\." = "southwest",
+    "\\bN\\.W\\." = "northwest",
+    "\\bNE\\." = "northeast",
+    "\\bSE\\." = "southeast",
+    "\\bSW\\." = "southwest",
+    "\\bNW\\." = "northwest",
+
+    "\\bN\\." = "north",
+    "\\bS\\." = "south",
+    "\\bE\\." = "east",
+    "\\bW\\." = "west",
+    "\\bC\\." = "central",
+
+    "\\bTemp\\." = "temperate",
+    "\\bTrop\\." = "tropical",
+    "\\bSubtrop\\." = "subtropical"
   )
 
-  # Apply replacements sequentially
-  for (pattern in names(replacements)) {
-    text <- gsub(pattern, replacements[pattern], text, perl = TRUE)
+  # Apply replacements, in order of decreasing pattern length
+  sorted_patterns <- names(replacements)[order(nchar(names(replacements)), decreasing = TRUE)]
+
+  for (pattern in sorted_patterns) {
+    text <- gsub(
+      pattern,
+      replacements[[pattern]],
+      text,
+      ignore.case = TRUE,
+      perl = TRUE
+    )
   }
 
-  # Replace ampersands with "and"
-  text <- gsub("\\s*&\\s*", " and ", text)
+  # Lowercase known full-form terms, only when they appear as whole words
+  for (term in lowercase_terms) {
+    pattern <- paste0("\\b", tools::toTitleCase(term), "\\b")
+    text <- gsub(pattern, term, text)
+  }
 
   return(text)
 }
+
